62335c021647f9ed43697cdbfd930dfc767a04cb
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-17 14:38:06 +0100

    create loading spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import './spinner.css'
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div class="loadingio-spinner-double-ring-e0znx624g5f">
+        <div class="ldio-8l2hiaq17g3">
+          <div></div>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
