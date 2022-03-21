712f97c2da47eae7a799e8805e61c78208dac81a
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-21 19:55:52 +0100

    person item-list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,20 +8,30 @@ import PersonDetails from "../person-details";
 import "./app.css";
 
 export default class App extends Component {
+  state = {
+    selectedPerson: null,
+  }
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+
   render() {
-    return( 
+    return (
       <div>
         <Header />
         <RandomPlanet />
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected}/>
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails personId={this.state.selectedPerson}/>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
